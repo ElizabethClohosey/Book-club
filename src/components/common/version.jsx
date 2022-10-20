ec97f79c923d4d9439530f7aaaef4a5c4fdc5001
ElizabethClohosey <Elizabethclohosey@gmail.com>
2022-10-20 18:33:24 -0400

    updated to prod version

--- src/components/common/version.jsx
+++ src/components/common/version.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Version = () => {
   return (
     <div className="version">
-      <p>v0.0.4</p>
+      {/* <p>v0.0.4</p> */}
+      <p>ProdTEST: v0.0.5</p>
     </div>
-    // <p>ProdTEST: v0.0.5</p>
   );
 };
 
