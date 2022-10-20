5480cc4c4f7362b3655d0fa7ebe840efce63a688
ElizabethClohosey <Elizabethclohosey@gmail.com>
2022-10-20 11:09:53 -0400

    added instructions to read list

--- src/components/read-list.jsx
+++ src/components/read-list.jsx
@@ -8,38 +8,50 @@ const ReadList = ({ readList }) => {
       <button className="list-btns" onClick={() => setShowList(!showList)}>
         {showList ? "Hide Read List" : "Show Read List"}
       </button>
-      {showList && (
-        <section className="read-list">
-          {Object.keys(readList).length > 0 ? (
-            readList.map((volume) => (
-              <div key={volume.id} className="book-card-wrapper">
-                <BookCard
-                  title={volume.volumeInfo.title}
-                  author={
-                    volume.volumeInfo.authors
-                      ? volume.volumeInfo.authors[0]
-                      : "N/A"
-                  }
-                  imgSrc={
-                    volume.volumeInfo.imageLinks &&
-                    volume.volumeInfo.imageLinks.thumbnail
-                      ? volume.volumeInfo.imageLinks.thumbnail
-                      : "#"
-                  }
-                />
-              </div>
-            ))
-          ) : (
-            <>
-              <p>
-                This list is empty <br />
-                Get started on your Book Bucket List!
-              </p>
 
-              <p></p>
-            </>
-          )}
-        </section>
+      {showList && (
+        <div className="list-wrapper">
+          <section className="list-section">
+            {Object.keys(readList).length > 0 ? (
+              <section>
+                <h3>Read List</h3>
+                <div className="list-instructions">
+                  <p>Page instructions will live here</p>
+                </div>
+                <hr className="dark" />
+                {readList.map((volume) => (
+                  <section className="read-list">
+                    <div key={volume.id} className="book-card-wrapper">
+                      <BookCard
+                        title={volume.volumeInfo.title}
+                        author={
+                          volume.volumeInfo.authors
+                            ? volume.volumeInfo.authors[0]
+                            : "N/A"
+                        }
+                        imgSrc={
+                          volume.volumeInfo.imageLinks &&
+                          volume.volumeInfo.imageLinks.thumbnail
+                            ? volume.volumeInfo.imageLinks.thumbnail
+                            : "#"
+                        }
+                      />
+                    </div>
+                  </section>
+                ))}
+              </section>
+            ) : (
+              <>
+                <p>
+                  This list is empty <br />
+                  Get started on your Book Bucket List!
+                </p>
+
+                <p></p>
+              </>
+            )}
+          </section>
+        </div>
       )}
     </section>
   );
